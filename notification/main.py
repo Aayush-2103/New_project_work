1490e4eb9517001b7967f37c7ce4e47242f32f8b
Aayush-2103 <2103aayush@gmail.com>
2025-05-27 19:19:42 +0530

    notification system link with main stream

--- notification/main.py
+++ notification/main.py
@@ -1,10 +1,19 @@
-def notify(cur, unique_id):
+def notify(unique_id):
     import os, sys
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     sys.path.append(base_dir)
 
     from datetime import datetime
     import tools.pull_data, tools.Mail.notify_mail
+    import mysql.connector as mysql
+
+    con = mysql.connect(
+        host = 'localhost',
+        user = 'root',
+        password = 'tiger',
+        database = 'userpf' 
+    )
+    cur = con.cursor()
    
     #check every minute to send notification mail if any
     past = datetime.now().strftime('%Y-%m-%d %H:%M')
@@ -20,10 +29,12 @@ def notify(cur, unique_id):
                 info = tools.pull_data.get_data_profile(cur, unique_id)
                 user_name = info[0]
                 user_mail = info[2]
-
-                print(now)
-
+                
                 if now == notify_time:
                     #notification sending mail has to be done.
                     tools.Mail.notify_mail.notification_mail(task_name, date_time, note, user_name, user_mail)
-        
+
+import sys
+unique_id = sys.argv[1]
+
+notify(unique_id)
